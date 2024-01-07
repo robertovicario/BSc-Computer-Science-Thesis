f08f5c9fefa2bf740c8857907d68026a211082c8
Roberto Vicario <rvicariodev@gmail.com>
2024-01-07 12:41:28 +0100

    updates

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,32 +2,32 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-def preprocess_data(file_path):
-    # Load data
+def preprocess_data(file_path, stats):
+    # Load data from csv file into a DataFrame
     data = pd.read_csv(file_path)
 
-    # Separate features (X) and labels (y)
-    X = data.iloc[:, :-1]
+    # Separate features (X) and target variable (y)
+    X = data.iloc[:, :-2]
     y = data.iloc[:, -1]
 
-    """print(X.describe())"""  # Data before preprocessing
-
-    # Encode 'condition' column
+    # Map 'condition' column to numeric values for easier processing
     condition_encoding = {'no stress': 0, 'time pressure': 1, 'interruption': 2}
     data['condition'] = data['condition'].map(condition_encoding)
 
-    # Standardize features
+    # Standardize features by removing the mean and scaling to unit variance
     scaler = StandardScaler()
     X_standardized = scaler.fit_transform(X)
 
-    # Apply PCA for dimensionality reduction
+    # Reduce dimensionality using PCA while retaining 95% of the variance
     pca = PCA(n_components=0.95, random_state=42)
     X_pca = pca.fit_transform(X_standardized)
+    
+    # Print statistical summary of data after preprocessing
+    print(pd.DataFrame(X_pca).describe())
 
-    """print(pd.DataFrame(X_pca).describe())""" # Data after preprocessing
+    if (stats):
+        # Print statistical summary of data before and after preprocessing
+        print(pd.DataFrame(X).describe())
+        print(pd.DataFrame(X_pca).describe())
 
     return X_pca, y
-
-# Preprocess training and test data
-X_train, y_train = preprocess_data('./data/training.csv')
-X_test, y_test = preprocess_data('./data/test.csv')
