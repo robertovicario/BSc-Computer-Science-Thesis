bd97085f124164b4df39d94d5f53c1db15e47567
Roberto Vicario <rvicariodev@gmail.com>
2024-01-09 21:29:55 +0100

    Update mlp.py

--- src/mlp.py
+++ src/mlp.py
@@ -4,10 +4,10 @@ from utils import X_test, X_train, evaluate_model, y_test, y_train
 #
 mlp = MLPClassifier(
     solver='adam',
-    learning_rate='adaptive',
-    hidden_layer_sizes=(50, 100, 50),
+    learning_rate='invscaling',
+    hidden_layer_sizes=(100,),
     alpha=0.001,
-    activation='tanh'
+    activation='logistic'
 )
 mlp.fit(X_train, y_train)
 
