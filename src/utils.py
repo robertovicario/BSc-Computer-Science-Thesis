02499e48f7771dd0d5c2d725c66296edc0f73a82
Roberto Vicario <rvicariodev@gmail.com>
2024-01-12 09:40:32 +0100

    updates

--- src/utils.py
+++ src/utils.py
@@ -12,12 +12,12 @@ def preprocess_data(file_path):
     data = pd.read_csv(file_path)
 
     #
-    X = data.iloc[:, :-2]
-    y = data.iloc[:, -1]
-
-    #
     condition_encoding = {'no stress': 0, 'time pressure': 1, 'interruption': 2}
     data['condition'] = data['condition'].map(condition_encoding)
+
+    #
+    X = data.iloc[:, :-2]
+    y = data.iloc[:, -1]
 
     #
     scaler = StandardScaler()
