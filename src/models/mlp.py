daee256bae35efe653f499be2adf231c67934c2c
Roberto Vicario <rvicariodev@gmail.com>
2024-01-25 16:08:16 +0100

    updates

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -2,13 +2,7 @@ from sklearn.neural_network import MLPClassifier
 from preprocessing import X_test, X_train, y_test, y_train
 from evaluation import evaluate_classifier
 
-mlp = MLPClassifier(
-    solver='adam',
-    learning_rate='invscaling',
-    hidden_layer_sizes=(100,),
-    alpha=0.001,
-    activation='logistic'
-)
+mlp = MLPClassifier()
 mlp.fit(X_train, y_train)
 
 evaluate_classifier(mlp, X_train, y_train, X_test, y_test, cv=3)
