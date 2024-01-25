6d596845c2e2668c63289714eedb9f3ca9482f77
Roberto Vicario <rvicariodev@gmail.com>
2024-01-25 16:36:03 +0100

    updates

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -2,7 +2,12 @@ from sklearn.neural_network import MLPClassifier
 from preprocessing import X_test, X_train, y_test, y_train
 from evaluation import evaluate_classifier
 
-mlp = MLPClassifier()
+mlp = MLPClassifier(
+    activation='tanh',
+    alpha=0.001,
+    hidden_layer_sizes=(50, 100, 50),
+    learning_rate='adaptive'
+)
 mlp.fit(X_train, y_train)
 
 evaluate_classifier(mlp, X_train, y_train, X_test, y_test, cv=3)
