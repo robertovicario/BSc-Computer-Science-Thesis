9acb20161b7c9bc21dae76d5edbef381c0d8a996
Roberto Vicario <rvicariodev@gmail.com>
2024-01-27 01:30:19 +0100

    updates

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -3,14 +3,6 @@ import seaborn as sns
 from sklearn.metrics import classification_report, confusion_matrix, pairwise_distances, silhouette_score
 from sklearn.model_selection import cross_val_score
 
-def plot_confusion_matrix(y_test, y_pred):
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    sns.heatmap(conf_matrix, annot=True, cmap='Blues')
-    plt.title('Confusion Matrix')
-    plt.xlabel('Predicted Label')
-    plt.ylabel('True Label')
-    plt.show()
-
 def evaluate_classification(model, X_train, y_train, X_test, y_test, cv):
     y_pred = model.predict(X_test)
     report = classification_report(y_test, y_pred)
@@ -19,7 +11,12 @@ def evaluate_classification(model, X_train, y_train, X_test, y_test, cv):
     cv_scores = cross_val_score(model, X_train, y_train, cv=cv, scoring='accuracy')
     print('cv_scores.mean:', f'{cv_scores.mean():.4f}')
 
-    plot_confusion_matrix(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    sns.heatmap(conf_matrix, annot=True, cmap='Blues')
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.show()
 
 def evaluate_clustering(X_train, labels):
     silhouette_avg = silhouette_score(X_train, labels)
--- src/models/kmeans.py
+++ src/models/kmeans.py
@@ -2,10 +2,10 @@ from sklearn.cluster import KMeans
 from evaluation import evaluate_clustering
 from preprocessing import X_train
 
-subset_size = int(0.2 * len(X_train))
+subset_size = int(0.01 * len(X_train))
 X_train_subset = X_train[:subset_size]
 
-kmeans = KMeans(n_clusters=2)
+kmeans = KMeans(n_clusters=3)
 kmeans.fit(X_train_subset)
 labels = kmeans.labels_
 
